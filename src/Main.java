fd491eef249b22967f5e9e35af9ffd293950df32
crisanpatrick <crisanpatrick@gmail.com>
2021-03-30 01:02:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("Patrick");
+
+        byte x = 10;
+        byte y = 3;
+        //Suma
+        System.out.print("Rezultatul sumei a doua numere este ");
+        System.out.println(x + y);
+        //Impartire
+        double a = 20;
+        double b = 48;
+        System.out.print("Rezultatul impartirii a doua numere este ");
+        System.out.println(a /= b);
+
+        // Operatiunea a) -5 + 8 * 6
+        System.out.print("Operatiunea a) -5 + 8 * 6 este egala cu ");
+        System.out.println(-5 + 8 * 6);
+        // Operatiunea b) (55+9) % 9
+        System.out.print("Operatiunea b) (55+9) % 9 este egala cu ");
+        System.out.println((55+9) % 9);
+        //Operatiunea c) 20 + -3*5 / 8
+        System.out.print("Operatiunea c) 20 + -3*5 / 8 este egala cu ");
+        System.out.println(20 + -3*5 / 8);
+        //Operatiunea d) 5 + 15 / 3 * 2 - 8 % 3
+        System.out.print("Operatiunea d) 5 + 15 / 3 * 2 - 8 % 3 este egala cu ");
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
+
+
+
