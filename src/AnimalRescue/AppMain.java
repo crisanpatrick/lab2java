5f63af108a4e18471cccd3d4054fa2977e3e1c95
crisanpatrick <crisanpatrick@gmail.com>
2021-04-01 21:05:45 +0300

    practica curs

--- src/AnimalRescue/AppMain.java
+++ src/AnimalRescue/AppMain.java
@@ -1,4 +1,11 @@
 package AnimalRescue;
 
 public class AppMain {
+    public static void main(String[] args) {
+        Dog rex = new Dog();
+        System.out.println(rex.breed);
+        rex.eat();
+        rex.weight = 25.9f;
+        System.out.println(rex.weight);
+    }
 }
--- src/AnimalRescue/Dog.java
+++ src/AnimalRescue/Dog.java
@@ -1,4 +1,20 @@
 package AnimalRescue;
 
 public class Dog {
+    String breed = "Ciobanesc German";
+    int age = 2;
+    String color = "Yellow/Black";
+    float weight = 25.4f;
+    int heightInCM = 50;
+    String name = "Rex";
+
+    public void bark(){
+        System.out.println("Ham!");
+    }
+    public void eat(){
+        System.out.println("Eating food!");
+    }
+    public void sleep(){
+        System.out.println("zZzZzZ *snores*");
+    }
 }
