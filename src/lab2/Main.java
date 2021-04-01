2ff78d9a40a272795cbb255918a96a6ef9ccd0cf
crisanpatrick <crisanpatrick@gmail.com>
2021-04-01 20:31:37 +0300

    practica curs

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -1,19 +1,32 @@
 package lab2;
 
+//inmultire, impartire, restul impartirii
 public class Main {
 
     public static void main(String[] args) {
-        float i = substract(22.5f, 34.5f, 67.9f);
-        System.out.println(i);
+        double div = div(69, 4);
+        System.out.println(div);
+        int mult = mult(3, 9);
+        System.out.println(mult);
+        int mod = mod(69,4);
+        System.out.println(mod);
     }
 
-    public static int sum(int first, int second) {
-        int sum = first + second;
-        return sum;
+
+    public static int mult(int first, int second) {
+        int mult = first * second;
+        return mult;
+
     }
 
-    public static float substract(float first, float second, float third) {
-        float result = first - second - third;
-        return result;
+    public static double div(int var1, int var2) {
+        int div = var1 / var2;
+        return div;
     }
+
+    public static int mod(int num1, int num2) {
+        int mod = (num1 %= num2);
+        return mod;
+    }
+
 }
