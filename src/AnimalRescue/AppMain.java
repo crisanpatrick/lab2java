58f40238f74f64da78a42dbdfe5f28f3d18b6652
crisanpatrick <crisanpatrick@gmail.com>
2021-04-02 13:13:57 +0300

    practica curs

--- src/AnimalRescue/AppMain.java
+++ src/AnimalRescue/AppMain.java
@@ -5,11 +5,13 @@ public class AppMain {
         Dog rex = new Dog();
         System.out.println(rex.breed);
         rex.eat();
-        System.out.print("He got fatter and his wiehgt is : " );
+        System.out.print("He got fatter and his weight is : " );
         rex.weight = (rex.weight + 1);
         System.out.print(rex.weight);
         System.out.println(" KG");
         System.out.println("After eating he got pig disease and went to sleep!");
         rex.sleep();
+        System.out.println("Rex woke up!");
+        rex.bark();
     }
 }
---
+++ src/AnimalRescue/Girl.java
@@ -0,0 +1,16 @@
+package AnimalRescue;
+
+import java.lang.reflect.AnnotatedArrayType;
+
+public class Girl {
+    String name = "Anna";
+    int money = 150;
+    String currency = "$";
+
+    public void wish(){
+        System.out.println("I wish I had a dog!");
+    }
+    public void doggo(){
+        System.out.println("Thank you! Now I have a dog friend!");
+    }
+}
