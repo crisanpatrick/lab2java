90a2eef000533ad95a687124fe76844d4953fa92
crisanpatrick <crisanpatrick@gmail.com>
2021-04-04 15:22:00 +0300

    practica curs

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -8,25 +8,74 @@ public class Main {
         System.out.println(div);
         int mult = mult(3, 9);
         System.out.println(mult);
-        int mod = mod(69,4);
+        int mod = mod(69, 4);
         System.out.println(mod);
+        int plus = plus(10, 8);
+        System.out.println(plus);
+        int sub = sub(78, 43);
+        System.out.println(sub);
+        int media = media(15, 58, 97);
+        System.out.println(media);
+        double celsius = celsius(198);
+        System.out.println(celsius);
+        double inch = inch(10);
+        System.out.println(inch);
     }
 
-
+    //Inmultire
     public static int mult(int first, int second) {
         int mult = first * second;
         return mult;
 
     }
 
+    //Impartire
     public static double div(int var1, int var2) {
         int div = var1 / var2;
         return div;
     }
 
+    //Restul impartirii
     public static int mod(int num1, int num2) {
         int mod = (num1 %= num2);
         return mod;
     }
+
+    //Adunare
+    public static int plus(int var1, int var2) {
+        int plus = (var1 + var2);
+        return plus;
+    }
+
+    //Scarede
+    public static int sub(int var1, int var2) {
+        int sub = (var1 - var2);
+        return sub;
+    }
+
+    //Media
+    public static int media(int var1, int var2, int var3) {
+        int media = ((var1 + var2 + var3) / 3);
+        return media;
+    }
+
+    //Conversie celsius
+    public static double celsius(int far) {
+        double celsius = ((5 / 9) * (far - 32));
+        return celsius;
+    }
+
+    //converise inch
+    public static double inch(int inc) {
+        double inch = (inc * 2.54);
+        return inch;
+    }
+
+    //viteza
+   // public static int speed(int meters, int hours, int minutes, int seconds) {
+   //     int speedinmps = (meters / (hours * 60 + minutes * 60 + seconds * 60));
+    //    int speedinkph =;
+    //    int speedinmph
+   // }
 
 }
