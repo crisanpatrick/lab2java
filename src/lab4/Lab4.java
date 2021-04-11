7f07359e2c5e367f678f4b4c38d0fc74cebc2ee3
crisanpatrick <crisanpatrick@gmail.com>
2021-04-11 15:42:52 +0300

    Animal Rescue Game && Homework

---
+++ src/lab4/Lab4.java
@@ -0,0 +1,16 @@
+package lab4;
+
+public class Lab4 {
+    public static void main(String[] args) {
+        LogicalOp op = new LogicalOp();
+//        op.countUp(4);
+//        op.countDown(5);
+//        op.countFromTo(5,50);
+//        op.countEven();
+//        op.countOdd();
+//        op.sumTo100(3);
+//        op.sumAndAverage(1);
+
+
+    }
+}
---
+++ src/lab4/LogicalOp.java
@@ -0,0 +1,59 @@
+package lab4;
+
+public class LogicalOp {
+    public void countUp(int number) {
+        for (int i = number; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public void countDown(int number) {
+        for (int i = number; i >= -100; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public void countFromTo(int var1, int var2) {
+        for (int i = var1; i <= var2; i++) {
+            System.out.println(i);
+        }
+    }
+
+    //    public void countFromSmallToBig(int var1, int var2){ }
+    public void countEven() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void countOdd() {
+        for (int i = 1; i <= 100; i++) {
+            if (!(i % 2 == 0)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public int sumTo100(int number) {
+        int sum = 0;
+        for (int i = number; i <= 100; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public float sumAndAverage(int number) {
+        float sum = 0;
+        int count = 0;
+        for (int i = number; i <= 100; i++) {
+            sum += i;
+            count++;
+        }
+        return sum / count;
+    }
+
+//    public void asterisk() {}
+
+}
